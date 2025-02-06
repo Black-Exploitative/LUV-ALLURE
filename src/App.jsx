7e8f897c5e21befb3a8244b2dbc4150ab9214a5c
Dearie <eburujacintha@gmail.com>
2025-02-06 13:43:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <p className="read-the-docs">
+        LUV ALLURE
+      </p>
+      <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
