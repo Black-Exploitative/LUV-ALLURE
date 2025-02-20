3556b676d280efa8633e308a5ba7aa9003860413
Dearie <eburujacintha@gmail.com>
2025-02-20 12:38:34 +0400

    added banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+const Banner = ({ imageUrl, title, description }) => {
+  return (
+    <div
+      className="relative w-full h-96 bg-cover bg-center"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      {/* Overlay for better text visibility */}
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-center p-4">
+        {title && (
+          <h1 className="text-white text-4xl font-bold mb-2">{title}</h1>
+        )}
+        {description && (
+          <p className="text-white text-lg max-w-2xl">{description}</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Banner.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+
+export default Banner;
