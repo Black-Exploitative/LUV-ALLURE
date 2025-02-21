545b2e673261fc48dca5969c25ae50392dc5438c
Dearie <eburujacintha@gmail.com>
2025-02-21 14:20:00 +0400

    updated cart provider context

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import ProductDetailsPage from "./pages/ProductDetailsPage"; 
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
+import { CartProvider } from "./context/CartContext"; 
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
+    <CartProvider>
     <Router>
+    <Navbar />
+    <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/product/:productId" element={<ProductDetailsPage />} />
       </Routes>
     </Router>
+    </CartProvider>
   );
 }
