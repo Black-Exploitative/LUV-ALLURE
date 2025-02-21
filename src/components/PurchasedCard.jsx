9bba76fb44abc49e3c22222d09ad8be7ca7cba94
Dearie <eburujacintha@gmail.com>
2025-02-21 10:04:19 +0400

    added the purchased card section

---
+++ src/components/PurchasedCard.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+
+const PurchasedCard = ({ product }) => {
+  const { name, price, sizes = [], images } = product;
+
+
+ 
+  return (
+    <div className={"p-4 relative group"}>
+      {/* Image Section */}
+      <div className="relative">
+        <img
+          src={images}
+          alt={name}
+          className="w-full ${imageHeightClass} object-cover"
+        />
+      </div>
+      {/* Product Details */}
+      <h3 className="text-lg font-semibold text-gray-900 mt-4">{name}</h3>
+      <p className="text-gray-700 text-lg mt-2">&#8358;{price}</p>
+      <div className="flex mt-4">
+        {sizes.map((size) => (
+          <div
+            key={size}
+            className="px-2 py-1 border border-gray-300 mr-2 mb-2 cursor-pointer hover:bg-gray-200"
+          >
+            {size}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+PurchasedCard.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    sizes: PropTypes.arrayOf(PropTypes.string),
+    images: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default PurchasedCard;
